f9c7941205bc21afba088f973b75d30b5d67edb8
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-05 13:27:04 +0200

    Implement start_controller logic in MenagerController

---
+++ Controllers/jerzy_controller.py
@@ -0,0 +1,18 @@
+from Models.mentor import Mentor
+from instances_manager import InstancesList
+
+class MenagerController(EmployeerCotroller):
+    def start_controller():
+
+        choice = None
+        while choice != "0":
+            mentors = Mentor.mentor_list
+            print_menu()
+            choice = get_choice()
+
+            if choice == "1":
+                person = Mentor()
+                InstancesList.add_person(mentors, person)
+
+            elif choice == "2":
+                InstancesList.remove_person(mentors, self.login)
