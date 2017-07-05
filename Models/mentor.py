1270b2dd2d231374d3130c2bdfb20ccf21b020a0
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-05 13:02:17 +0200

    Change of class attribute in Mentor class in Models/mentor.py

--- Models/mentor.py
+++ Models/mentor.py
@@ -2,4 +2,4 @@ from Models.codecooler import Codecooler
 from Controllers.instances_manager import InstancesList
 
 class Mentor(Codecooler):
-    mentor_list = InstancesList()
+    mentor_list = []
