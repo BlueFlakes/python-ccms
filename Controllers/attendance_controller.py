a3a3646b100ce8db95ea394b8726fe720d9b8c0a
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-06 14:09:48 +0200

    Implement check_attendance and get_attendnace_listmethods, update of start_controller and calculate_attendnace in AttendanceController

--- Controllers/attendance_controller.py
+++ Controllers/attendance_controller.py
@@ -1,22 +1,54 @@
 from Models.attendance import AttendanceModel
 from View.codecooler_view import CodecoolerView
 
+
 class AttendanceController:
 
-    def start_controller(self, students):
-        student_detail = CodecoolerView.get_inputs("Check attendance", ["Student idx", "Attendance state"])
-        attendance = AttendanceModel(student_detail[0], student_detail[1])
+    def start_controller(students):
+        option = 0
+        while not option == "0":
+            os.system("clear")
 
-        for student in students:
-            if student.idx == student_idx:
-                student.attendance_list.append(attendance)
-                attendance_procent = self.calculate_attendnace(student)
-                print(attendance_procent)
+            CodecoolerView.print_menu("Welcome {} {}".format(name, surname),
+                                      ["Check attendnace", "View student attendance"], "Exit")
+            options = CodecoolerView.get_inputs("Please choose a number", ["Number"])
+            option = options[0]
+
+            if option == "1":
+                self.check_attendance(students)
+            elif option == "2":
+                pass
+
 
     @staticmethod
-    def calculate_attendnace(student):
+    def calculate_attendnace(students_attendance, student_idx):
         attendance_sum = 0
-        for index in range(len(attendance_list)):
-            attendance_sum += attendance_list[index].state
+        for attendance in students_attendance:
+            if attendance[0] == student_idx:
+                attendance_sum += attendance[1]
 
-        return attendance_sum/len(attendance_list)
+            return round((attendance_sum/len(attendance_list)*100), 2)
+
+    def check_attendance(self, students):
+            for student in students:
+                student_detail = CodecoolerView.get_inputs("Check attendance", ["Attendance state"])
+                attendance = AttendanceModel(student[0], student_detail[1])
+
+                students_attendance = DataManager.read_file("csv/attendance.csv")
+
+                students_attendance.append(attendance)
+
+                attendance_procent = self.calculate_attendnace(students_attendance, student[0])
+                print("Student attendance {}%".format(attendance_procent))
+
+                DataManager.save_file("csv/attendance.csv", students)
+
+    @staticmethod
+    def get_attendnace_list():
+        attendance_list = []
+        choosen_student = CodecoolerView.get_inputs("Student idx", ["Student idx"])
+
+        for attendance in students_attendance:
+            if attendance[0] == student_idx:
+                attendance_list.append(attendance)
+
