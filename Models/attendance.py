2afbe9b595bb6014edef7be23a3559ba54d9d5f9
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-06 22:44:36 +0200

    Docstring for AttendanceModel class done

--- Models/attendance.py
+++ Models/attendance.py
@@ -1,8 +1,21 @@
 
 
 class AttendanceModel:
+    """This class represents attendance of students on lessons"""
 
     def __init__(self, student_idx, date, state):
+        """
+        Constructor of attendance object.
+
+        Args:
+            student_idx (string): uniqe student ide
+            date (:obj: `datetime.date`): date of cheak attendance
+            state (int): status of student attendane
+
+        Exmaples:
+            There are three states for attendance: 0 - not present, 0.5 - late
+            and 1 - present
+        """
         self.student_idx = student_idx
         self.date = date
         self.state = state
