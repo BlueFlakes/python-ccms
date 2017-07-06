f2c9025cbccc0bdf65ec91b97b5dbef148a39b0f
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-06 22:52:49 +0200

    Docstring in Manager class done

--- Models/manager.py
+++ Models/manager.py
@@ -2,4 +2,11 @@ from Models.employee import Employee
 
 
 class Manager(Employee):
+    """
+    This class represents Codecoole manager
+
+    Attributes:
+        manager_list (list): list of Codecool managers
+    """
+
     manager_list = []
