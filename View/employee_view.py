58bd5d4cbb28921499a5749d2d5ff639382e6b5a
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-06 23:22:07 +0200

    Docstring in EmployeeView class done

--- View/employee_view.py
+++ View/employee_view.py
@@ -2,6 +2,15 @@ from View.codecooler_view import CodecoolerView
 
 
 class EmployeeView(CodecoolerView):
+    """This class contains logic for prints of student list"""
+
     def print_student_list(students):
+        """
+        Prints list of students
+
+        Args:
+            students (list of :obj: `Student`): list with all students
+        """
+
         for student in students:
             print("{}, {}, {}".format(student.idex, student.name, student.surname))
