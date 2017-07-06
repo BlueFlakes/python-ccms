82b443a5668de1f66697ea5a63a9a52b67f993e8
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-06 14:08:27 +0200

    Change order of attributes in AttendnaceModel

--- Models/attendance.py
+++ Models/attendance.py
@@ -4,6 +4,6 @@ from datetime import date
 class AttendanceModel:
 
     def __init__(self, student_idx, state):
-        self.date = date.today()
         self.student_idx = student_idx
+        self.date = date.today()
         self.state = state
