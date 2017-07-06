11e8af1611819fdf1216b3c3cf0d6c51f53a04c0
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-06 11:29:52 +0200

    Change state attribute in init of AttendanceModel class

--- Models/attendance.py
+++ Models/attendance.py
@@ -3,7 +3,7 @@ from datetime import date
 
 class AttendanceModel:
 
-    def __init__(self, student_idx):
+    def __init__(self, student_idx, state):
         self.date = date.today()
         self.student_idx = student_idx
-        self.state = 0
+        self.state = state
