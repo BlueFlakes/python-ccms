0b996c062e016185efa43b27bcb99ce33f9308e4
JakubJaniszewski <cwanykuba@gmail.com>
2017-07-07 09:44:42 +0200

    Add check_given_grade method

--- Controllers/employee_controller.py
+++ Controllers/employee_controller.py
@@ -1,4 +1,5 @@
 from Models.student import Student
+from View.employee_view import EmployeeView
 
 
 class EmployeeController():
@@ -22,8 +23,30 @@ class EmployeeController():
             is_correct = False
 
         while not is_correct:
-            email = input("Please provide correct email: ")
+            email = EmployeeView.single_input("Please provide correct email: ")
             if "@" in email and "." in email:
                 is_correct = True
 
         return email
+
+    @staticmethod
+    def check_given_grade(grade):
+        is_correct = EmployeeController.try_str_convert(grade)
+
+        while not is_correct:
+            grade = EmployeeView.single_input("Please provide grade from 1 to 100: ")
+            is_correct = EmployeeController.try_str_convert(grade)
+
+        return grade
+
+    @staticmethod
+    def try_str_convert(mssg):
+        try:
+            if int(mssg) > 0 and int(mssg) < 101:
+                is_correct = True
+            else:
+                is_correct = False
+        except ValueError:
+            is_correct = False
+
+        return is_correct
