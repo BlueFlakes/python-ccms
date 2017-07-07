1c1c728ce9dd5e163373dd3b8bcbd03b416288aa
JakubJaniszewski <cwanykuba@gmail.com>
2017-07-07 02:29:12 +0200

    Add check_given_email method

--- Controllers/employee_controller.py
+++ Controllers/employee_controller.py
@@ -13,3 +13,17 @@ class EmployeeController():
             :obj: list of :obj: `Student`: list off students
         """
         return Student.student_list
+
+    @staticmethod
+    def check_given_email(email):
+        if "@" in email and "." in email:
+            is_correct = True
+        else:
+            is_correct = False
+
+        while not is_correct:
+            email = input("Please provide correct email: ")
+            if "@" in email and "." in email:
+                is_correct = True
+
+        return email
