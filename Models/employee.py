8228a345589b90eccae1dc2344af7a21f8f8e8cd
JakubJaniszewski <cwanykuba@gmail.com>
2017-07-07 04:14:28 +0200

    Delete not needed file

--- Models/employee.py
+++
@@ -1,6 +0,0 @@
-from Models.codecooler import Codecooler
-
-
-class Employee(Codecooler):
-    """This abstract class represents Codecoole employee """
-    pass
--- View/student_view.py
+++
@@ -1,2 +0,0 @@
-class StudentView:
-    pass
