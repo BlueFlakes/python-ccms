df11cb29c2ff1e10483b0723ba56b91a645e8045
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-07 09:24:13 +0200

    Docstrings in AssignmentController done

--- Controllers/assignment_controller.py
+++ Controllers/assignment_controller.py
@@ -4,9 +4,11 @@ from data_manager import DataManager
 
 
 class AssignmentController:
+    """Contain methods to work on Attendance object"""
 
     @classmethod
     def start_controller(cls):
+        """Contain main logic for AttendanceController. Ask user about assigemnt details"""
         assgn = CodecoolerView.get_inputs("Add assignment", ["Title", "Description"])
         Assignment.assignments.append(Assignment(assgn[0], assgn[1]))
 
@@ -14,4 +16,5 @@ class AssignmentController:
 
     @staticmethod
     def save_assignment(assgn):
+        """Save list of assignments in csv file"""
         DataManager.extend_file("csv/assignments.csv", assgn)
