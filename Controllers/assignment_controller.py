d131cf6248f7201bb4da90cc4cecfd00154c6c50
JakubJaniszewski <cwanykuba@gmail.com>
2017-07-18 09:45:58 +0200

    Make is_empty_input() class method

--- Controllers/assignment_controller.py
+++ Controllers/assignment_controller.py
@@ -12,11 +12,16 @@ class AssignmentController:
         Contain main logic for AssignmentController.
         Ask user about assigemnt details and add it to assigments list
         """
+        is_empty_input = True
 
-        assgn = CodecoolerView.get_inputs("Add assignment", ["Title", "Description"])
-        Assignment.assignments.append(Assignment(assgn[0], assgn[1]))
+        while is_empty_input:
+            assgn_details = CodecoolerView.get_inputs("Add assignment", ["Title", "Description"])
+            is_empty_input = AssignmentController.is_empty_input(assgn_details)
 
-        cls.save_assignment(assgn)
+
+        Assignment.assignments.append(Assignment(assgn_details[0], assgn_details[1]))
+
+        cls.save_assignment(assgn_details)
 
     @staticmethod
     def save_assignment(assgn):
@@ -28,3 +33,13 @@ class AssignmentController:
 
         """
         DataManager.extend_file("csv/assignments.csv", assgn)
+
+    @classmethod
+    def is_empty_input(cls, assgn_details):
+
+        for answer in assgn_details:
+            if len(answer) < 1:
+                CodecoolerView.print_result("You can't make assignment without title or description!")
+                return True
+
+        return False
