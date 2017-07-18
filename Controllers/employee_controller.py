37b7b62ae86bbdd71e32060f46828894d22b2671
Kamil <blueflamesxcode@gmail.com>
2017-07-18 14:15:35 +0200

    deleted useless validation for grades

--- Controllers/employee_controller.py
+++ Controllers/employee_controller.py
@@ -10,25 +10,3 @@ def get_student_list():
         :obj: list of :obj: `Student`: list off students
     """
     return Student.student_list
-
-
-def check_given_grade(grade):
-    is_correct = try_str_convert(grade)
-
-    while not is_correct:
-        grade = employee_view.single_input("Please provide grade from 1 to 100: ")
-        is_correct = try_str_convert(grade)
-
-    return grade
-
-
-def try_str_convert(mssg):
-    try:
-        if int(mssg) > 0 and int(mssg) < 101:
-            is_correct = True
-        else:
-            is_correct = False
-    except ValueError:
-        is_correct = False
-
-    return is_correct
