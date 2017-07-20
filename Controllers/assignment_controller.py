9a0c7f7d10e27581434c6711afba40948e992c20
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-20 23:29:33 +0200

    Add docstrings to is_empty_input(), _create_student_assigments(), set_deadline(), check_date() functions in assignment_controller module

--- Controllers/assignment_controller.py
+++ Controllers/assignment_controller.py
@@ -12,6 +12,7 @@ def start_controller(students):
     Contain main logic for AssignmentController.
     Ask user about assigemnt details and add it to assigments list
     """
+
     "is_empty = True"
     questions = ["Title", "Description"]
     deadline_questions = ["Deadline day", "Deadline month", "Deadline year"]
@@ -36,6 +37,15 @@ def start_controller(students):
 
 
 def is_empty_input(assgn_details):
+    """
+    Validate if created assigment is empty
+
+    Args:
+        assgn_details (list of strings): title and description of assigment
+
+    Returns:
+        bool: True if no assigment details given, otherwise False
+    """
 
     for answer in assgn_details:
         if len(answer) < 1:
@@ -45,18 +55,46 @@ def is_empty_input(assgn_details):
 
 
 def _create_student_assigments(students, title):
+    """
+    Create SubmitAssigment object with no return date and link to github for evry Student object.
+
+    Args:
+        students (list of :obj: `Student`): list of all students
+        title (string): title of assigment
+    """
 
     for student in students:
         student_assigment = SubmitAssignment(student.idx, "empty", title, "empty")
         SubmitAssignment.add_assignment(student_assigment)
 
+
 def set_deadline(d_day, d_month, d_year):
+    """
+    Create date of deadline
+
+    Args:
+        d_day (str): day of deadline
+        d_month (str): month of deadline
+        d_year (str): year of deadline
+
+    Returns:
+        :obj: `date`: date of deadline
+    """
+
     deadline = date.today()
     deadline = deadline.replace(day=int(d_day), month=int(d_month), year=int(d_year))
     check_date(deadline)
 
     return deadline
 
+
 def check_date(deadline):
+    """
+    Validete if assigment deadline have future date.
+
+    Args:
+        deadline (:obj: `date`): date of deadline
+    """
+
     if deadline == date.today() or deadline < date.today():
         raise ValueError
