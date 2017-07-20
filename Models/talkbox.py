d5c47c8a50e1da74ee3611835b8a4d9cf7dbc917
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-21 01:30:44 +0200

    Add docstrings in TalkBox class

--- Models/talkbox.py
+++ Models/talkbox.py
@@ -1,25 +1,58 @@
 from Data import tools
 from Models.message import Message
 
+
 class TalkBox:
+    """
+    This class represents Talkbox
+
+    Attributes:
+        talkbox (:obj: `TalkBox`): list of masseges
+        _file_name (string): name of file to open
+    """
+
     talkbox = []
     _file_name = 'csv/talkbox.csv'
 
     @classmethod
     def get_talkbox(cls):
+        """
+        Returns:
+            list of :obj: `TalkBox`: list of all masseges
+        """
+
         return cls.talkbox
 
     @classmethod
     def add_message(cls, author, sentence):
+        """
+        Inser Message object to talkbox and leave only last twenty.
+
+        Args:
+            author (string): author of massege
+            sentence (string): text of massege
+        """
+
         cls.talkbox.insert(0, Message(author, sentence))
         cls.talkbox = cls.talkbox[:20]
 
     @classmethod
     def load_talkbox(cls):
+        """
+        Call function to open csv file and convert it rows to Talkbox objects
+        """
+
         cls.talkbox = tools.get_data_from_file(cls._file_name, Message)
 
     @classmethod
     def get_records_from_objects(cls):
+        """
+        Get specified attributes from object
+
+        Returns:
+            list of lists: list with attributes of all Message objects
+        """
+
         temp = []
 
         for message in cls.talkbox:
@@ -29,5 +62,9 @@ class TalkBox:
 
     @classmethod
     def save_talkbox(cls):
+        """
+        Call functions to save Talkbox to csv file
+        """
+
         talkbox_data_records = cls.get_records_from_objects()
         tools.save_data_to_file(cls._file_name, talkbox_data_records)
